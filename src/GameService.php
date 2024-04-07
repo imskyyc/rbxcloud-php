56be7db45f2d73a6e8d6403b6f07f8df6658af55
imskyyc <sky@imskyyc.com>
2024-04-06 20:25:13 -0700

    add cookie field

--- src/GameService.php
+++ src/GameService.php
@@ -3,11 +3,12 @@
 namespace Imskyyc\RbxcloudPhp;
 
 class GameService {
-    public static function getPlaceDetails(String $placeId) 
+    public static function getPlaceDetails(String $placeId, String $userCookie) 
     {
         $curlHandle = curl_init();
 
         curl_setopt($curlHandle, CURLOPT_URL, "https://games.roblox.com/v1/games/multiget-place-details?placeIds=$placeId");
+        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, "Cookie: .ROBLOSECURITY=$userCookie");
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
 
         $response = curl_exec($curlHandle);
